28eacccf5c8b3696827914f5231ab397f0129b6e
MayurJen <mayur.pallavarapu@in.abb.com>
2017-05-02 16:40:00 +0530

    Add files via upload

---
+++ App5/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class App5
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        double number = Convert.ToDouble(input);
+        for (int i = 1; i < 10; i++)
+        {
+            double result = number * i;
+            Console.WriteLine("{0} x {1,2} = {2,2}",number,i,result);
+        }
+    }
+}
